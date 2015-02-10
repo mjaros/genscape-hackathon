253f6a4e3d0bb9b1dda39dd11e7435dab1c5bfdc
Matthias Jaros <mjaros@mjaros.com>
2015-02-09 21:37:20 -0500

    Add json response example and run server on all interfaces

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
+# Template rendering example
 @app.route("/hello")
 @app.route("/hello/<name>")
 def hello(name=None):
   return render_template('hello.html', name=name)
+
+# JSON response example
+@app.route("/json")
+def json():
+  return jsonify(first_name='John', last_name='Wayne')
 
 if __name__ == "__main__":
   # This will enable hot reloading, so you don't need to restart each time
@@ -14,4 +21,4 @@ if __name__ == "__main__":
   # MAKE SURE TO DISABLE IN PRODUCTION
   app.debug = True
 
-  app.run()
+  app.run(host='0.0.0.0')
